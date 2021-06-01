ea5f504c503457c23a60bc1e0d76233f68a0d73e
99brgs@gmail.com <99brgs@gmail.com>
2021-06-01 12:17:13 +0200

    Merge conflicts resolved

---
+++ plugins/arCasPlugin/modules/cas/actions/logoutAction.class.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class CasLogoutAction extends sfAction
+{
+  public function execute($request)
+  {
+    if (!isset($request->slug))
+    {
+      $this->forward404();
+    }
+
+    // Check slug existence
+    $sql = 'SELECT s.id FROM slug s
+      JOIN object o ON s.object_id = o.id
+      WHERE s.slug = ? AND o.class_name IN (?, ?, ?)
+      AND o.id NOT IN (?, ?, ?)';
+
+    $slugId = QubitPdo::fetchColumn(
+      $sql,
+      array(
+        $request->slug,
+        'QubitInformationObject',
+        'QubitActor',
+        'QubitRepository',
+        QubitInformationObject::ROOT_ID,
+        QubitActor::ROOT_ID,
+        QubitRepository::ROOT_ID
+      )
+    );
+
+    if ($slugId === false)
+    {
+      $this->forward404();
+    }
+
+    // This shouldn't be reached, as logout redirects to the CAS server.
+    throw new Exception("CAS logout failed");
+  }
+}
